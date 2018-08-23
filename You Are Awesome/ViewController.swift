19975976435f497ce6d9c9b403ac5394bda338d1
benmeisenzahl <benny.meisenzahl@gmail.com>
2018-08-23 10:38:48 -0400

    Initial Commit

---
+++ You Are Awesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome
+//
+//  Created by Ben Meisenzahl on 8/23/18.
+//  Copyright © 2018 Ben Meisenzahl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
